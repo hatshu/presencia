efc1cc9115a0fbb3f54bce761d08b63098a508f3
bmartin <bmartin@localhost>
2018-01-22 14:16:49 +0100

    Conexion con la base de datos

--- WpfSQLite/View/Start.xaml.cs
+++ WpfSQLite/View/Start.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Configuration;
+using System.Data;
+using System.Security.Cryptography;
 using System.Windows.Controls;
 
 namespace Presencia.View
@@ -11,7 +16,23 @@ namespace Presencia.View
       public Start()
       {
          InitializeComponent();
+         cargaCombobox();
       }
+
+      private static void cargaCombobox()
+      {
+         string conexionString = ConfigurationManager.ConnectionStrings["ConexionPruebas"].ConnectionString;
+         using (SqlConnection conn = new SqlConnection(conexionString))
+         {
+            conn.Open();
+            MessageBox.Show(conn.DataSource);
+            DataTable dt = new DataTable();
+            SqlCommand consultaUsuarios = new SqlCommand("SELECT * FROM tb_Users", conn );
+            conn.Close();
+         }
+         ;
+      }
+
 
       private void Back_OnClick(object sender, RoutedEventArgs e)
       {
