6205c0d45009514a0876141a5f35398df2c3f793
bmartin <bmartin@localhost>
2018-02-08 09:41:28 +0100

    Nueva clase para obtener ordenamiento de fechas pero que sea un string para que solo salga el dia

---
+++ WpfSQLite/Model/ElementoListaResumenFinal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presencia.Model
+{
+   class ElementoListaResumenFinal
+   {
+      public string Nombre { get; set; }
+      public string Dia { get; set; }
+      public string Entrada { get; set; }
+      public string Salida { get; set; }
+      public string Ausencia { get; set; }
+      public string Aus_Entrada { get; set; }
+      public string Aus_Salida { get; set; }
+      public string HorasEnCentro { get; set; }
+      public string Comentarios { get; set; }
+
+      //Para que se detecte cuando se cambia cada elemento
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      private void NotifyPropertyChanged(string info)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+      }
+   }
+}
